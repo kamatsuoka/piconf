0babefa9bcf97223477f1eebbd310db41c7256ec
Kenji Matsuoka <kenji@alexa.com>
2016-01-13 22:00:37 -0800

    using a pool of workers to compress jpgs

--- upload.py
+++ upload.py
@@ -6,21 +6,28 @@
 # - upload to Amazon Cloud Drive
 # - delete original and resized version
 
-import glob, os, subprocess, time
+from glob import glob
+import os, subprocess, time
+from multiprocessing import Pool
+
+def compress(f):
+    """ compresses a jpg to a smaller file in the 'converted' dir and deletes the original """
+    convert_opts = '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%'
+    cmd = '/usr/bin/convert %s %s converted/%s' % (convert_opts, f, f)
+    returncode = subprocess.call(cmd.split(' '), timeout=5)
+    if returncode == 0:
+        os.remove(f)
 
 if __name__ == '__main__':
     source_dir = '/dev/shm/motion_capture'
-    compressed_dir = '%s/compressed' % source_dir
+    compressed_subdir = '%s/compressed' % source_dir
     upload_dir = '/Pictures/picam'
 
     os.chdir(source_dir)
 
-    while True:
-        for f in glob.glob("*.jpg"):
-            cmd = '/usr/bin/convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85%% %s converted/%s' % (f, f)
-            returncode = subprocess.call(cmd.split(' '), timeout=5)
-            if returncode == 0:
-                os.remove(f)
+    with Pool(processes=4) as pool:
+        while True:
+            pool.map(compress, glob("*.jpg"), 1)
         time.sleep(1)
                 
         
