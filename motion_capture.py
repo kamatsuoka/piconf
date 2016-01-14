cb7fb421e1d63b44c9c32452b47b16c8b48c1fe3
Kenji Matsuoka <kenjim@gmail.com>
2016-01-13 21:04:43 -0800

    motion detection -> preview -> image capture

---
+++ motion_capture.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3 
+
+import numpy as np
+import picamera
+import picamera.array
+import datetime
+import time
+
+class DetectMotion(picamera.array.PiMotionAnalysis):
+
+    def __init__(self, camera, motion_threshold, min_blocks, still_interval, size = None):
+        self.start_time = time.time()
+        self.last_motion = None
+        self.last_recordable = None
+        self.warmed_up = False
+        self.still_interval = still_interval
+        self.preview_running = False
+        super().__init__(camera, size)
+
+    def analyse(self, a):
+        if self.warmed_up:
+            a = np.sqrt(
+                np.square(a['x'].astype(np.float)) +
+                np.square(a['y'].astype(np.float))
+            ).clip(0, 255).astype(np.uint8)
+            # If there're more than 10 vectors with a magnitude greater
+            # than 60, then say we've detected motion
+            moving = (a > motion_threshold).sum()
+            t = time.time()
+
+            # Turn on the display at the slightest sign of motion,
+            # then turn it off after 7 seconds of no motion
+            if moving > 1:
+                self.last_motion = t
+                if not self.preview_running:
+                    camera.start_preview()
+                    self.preview_running = True
+            elif self.preview_running and self.last_motion is not None and t - self.last_motion > 7:
+                camera.stop_preview()
+                self.preview_running = False
+
+            # If at least min_blocks are moving, signal that we can start recording.
+            if moving > min_blocks:
+                self.last_recordable = t
+            else:
+                if (self.last_recordable is not None and
+                    t - self.last_recordable > 2 * self.still_interval):
+                    self.last_recordable = None
+        else:
+            # Warmup time needed to prevent spurious recording at start
+            self.warmed_up = time.time() - self.start_time > 2
+
+
+if __name__ == '__main__':
+    wait_interval = 0.1 # how long to wait between checking for motion
+    still_interval = 0.5 # min seconds between still frames
+    framerate = 15
+    camera_width = 1296
+    camera_height = 730
+    motion_width = 432
+    motion_height = 243
+    motion_threshold = 7
+    min_blocks = 4
+    quality = 85
+    iso = 800
+    hflip = True
+    vflip = False
+
+    last_still = time.time()
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (camera_width, camera_height)
+        camera.framerate = framerate
+        camera.hflip = hflip
+        camera.vflip = vflip
+        camera.iso = iso
+        with DetectMotion(camera, motion_threshold, min_blocks, still_interval,
+                          size=(motion_width, motion_height)) as output:
+            camera.start_recording('/dev/null', resize=(motion_width, motion_height),
+                                   format='h264', motion_output=output)
+            while True:
+                camera.wait_recording(wait_interval)
+                if (output.last_recordable is not None
+                    and time.time() - last_still >= still_interval):
+                    time_now = datetime.datetime.now()
+                    filename = 'picam-%s.jpg' % time_now.strftime('%FT%H-%M-%S.%f')
+                    camera.capture(filename, use_video_port=True, quality = quality)
+                    last_still = time.time()
+            camera.stop_recording()
