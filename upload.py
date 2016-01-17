db596b494fa98929b23499d94e21242f6a178e34
Kenji Matsuoka <kenji@alexa.com>
2016-01-16 17:23:47 -0800

    working with multiprocesses

--- upload.py
+++ upload.py
@@ -8,28 +8,45 @@
 
 from glob import glob
 import os, subprocess, time
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
+import signal
+import sys
+import time
 
-def compress(f):
+# continually looks for files in captured_dir and converts them to a smaller size.
+
+def init_worker():
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+def compress(filename):
     """ compresses a jpg to a smaller file in the 'converted' dir and deletes the original """
     convert_opts = '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%'
-    cmd = '/usr/bin/convert %s %s converted/%s' % (convert_opts, f, f)
+    converted_filename = filename.replace('/captured/', '/converted/')
+    cmd = 'convert %s %s %s' % (convert_opts, filename, converted_filename)
     returncode = subprocess.call(cmd.split(' '), timeout=5)
     if returncode == 0:
-        os.remove(f)
+        os.remove(filename)
 
 if __name__ == '__main__':
-    source_dir = '/dev/shm/motion_capture'
-    compressed_subdir = '%s/compressed' % source_dir
-    upload_dir = '/Pictures/picam'
+    base_dir = sys.argv[1] if len(sys.argv) > 1 else '/dev/shm/motion_capture'
+    captured_dir = '%s/captured' % base_dir
+    converted_dir = '%s/converted' % base_dir
+    for dir in [ base_dir, captured_dir, converted_dir ]:
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
 
-    os.chdir(source_dir)
+    pool = Pool(processes=4, initializer=init_worker)
 
-    with Pool(processes=4) as pool:
+    try:
         while True:
-            pool.map(compress, glob("*.jpg"), 1)
-        time.sleep(1)
-                
-        
-        
+            pool.map(compress, glob('%s/*.jpg' % captured_dir), 1)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print('Interrupted')
+        pool.terminate()
+        pool.join()
+    else:
+        print('Exiting')
+        pool.close()
+        pool.join()
     
