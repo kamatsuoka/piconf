07fdd446342e92876f219f6cccdbf1cc6c7baa11
kamatsuoka <kenjim@gmail.com>
2016-01-18 12:00:25 -0800

    bugfixes

--- opt/motion_capture/bin/compress.py
+++ opt/motion_capture/bin/compress.py
@@ -5,6 +5,8 @@
 # - compress to a smaller size 
 # - delete original
 
+
+from concurrent.futures import ThreadPoolExecutor
 from dateutil import parser
 from glob import glob
 import os
@@ -13,6 +15,7 @@ import re
 import subprocess
 import sys
 import time
+import traceback
 
 datepattern = re.compile('\d{4}-\d{2}-\d{2}T')
 
@@ -24,20 +27,24 @@ def filedate(path):
 def compress(path):
     """ compresses a jpg to a smaller file in the 'compressed' dir,
         then moves the compressed file to the upload dir and deletes the original """
-    convert_opts = '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%'
-    compressed_path = path.replace('/captured/', '/compressed/')
-    cmd = 'convert %s %s %s' % (convert_opts, path, compressed_path)
-    returncode = subprocess.call(cmd.split(' '), timeout=5)
-    if returncode == 0:
-        filename = basename(path)
-        (y, m, d) = filedate(filename)
-        upload_basedir = dirname(compressed_path).replace('/compressed', '/upload')
-        upload_dir = '%s/%d/%02d/%02d' % (upload_basedir, y, m, d)
-        os.makedirs(upload_dir, exist_ok=True)
-        upload_path = '%s/%s' % (upload_dir, filename)
-        print('renaming %s to %s' % (compressed_path, upload_path))
-        os.rename(compressed_path, upload_path)
-        os.remove(path)
+    try:
+        convert_opts = '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%'
+        compressed_path = path.replace('/captured/', '/compressed/')
+        cmd = 'convert %s %s %s' % (convert_opts, path, compressed_path)
+        returncode = subprocess.call(cmd.split(' '), timeout=5)
+        if returncode == 0:
+            filename = basename(path)
+            (y, m, d) = filedate(filename)
+            upload_basedir = dirname(compressed_path).replace('/compressed', '/upload')
+            upload_dir = '%s/%d/%02d/%02d' % (upload_basedir, y, m, d)
+            os.makedirs(upload_dir, exist_ok=True)
+            upload_path = '%s/%s' % (upload_dir, filename)
+            print('renaming %s to %s' % (compressed_path, upload_path))
+            os.rename(compressed_path, upload_path)
+            os.remove(path)
+    except:
+        print("Unexpected error: %s " % traceback.format_exc())
+        
 
 if __name__ == '__main__':
     base_dir = sys.argv[1] if len(sys.argv) > 1 else '/dev/shm/motion_capture'
@@ -51,14 +58,10 @@ if __name__ == '__main__':
         while True:
             captured_files = glob('%s/*.jpg' % captured_dir)
             start_time = time.time()
-            for path in captured_files:
-                try:
-                    compress(path)
-                except:
-                    print("Unexpected error:", sys.exc_info()[0])
+            with ThreadPoolExecutor(max_workers=4) as executor:
+                executor.map(compress, captured_files, timeout=60)
             if len(captured_files) > 0:
                 print("Converted %s files in %s seconds" % (len(captured_files), time.time() - start_time))
-                    
             time.sleep(1)
     except KeyboardInterrupt:
         print('Interrupted')
--- opt/motion_capture/bin/upload.py
+++ opt/motion_capture/bin/upload.py
@@ -5,10 +5,11 @@
 from datetime import date
 from glob import glob
 import os
-import shutils
+import shutil
 import subprocess
 import sys
 import time
+import traceback
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -16,30 +17,30 @@ def upload(dir):
     """ uploads files in a directory tree, then deletes them.  
         dir is expected to be a year, with month and day directories under it."""
     try:
-        cmd = 'acd_cli upload --remove-source-files --max-connections=4 %s Pictures/picam'
-        return subprocess.call(cmd.split(' '), timeout=20)
+        acd_opts = '--force --remove-source-files --max-connections=8'
+        cmd = 'acd_cli upload %s %s Pictures/picam' % (acd_opts, dir)
+        return subprocess.call(cmd.split(' '), timeout=60)
     except:
-        print("Unexpected error:", sys.exc_info()[0])
-
-def         
+        print("Unexpected error: %s " % traceback.format_exc())
 
 if __name__ == '__main__':
     base_dir = sys.argv[1] if len(sys.argv) > 1 else '/dev/shm/motion_capture'
     upload_dir = '%s/upload' % base_dir
     for dir in [ base_dir, upload_dir ]:
-        os.mkdirs(dir, exist_ok=True)
+        os.makedirs(dir, exist_ok=True)
 
     while True:
         current_year = date.today().year
         for dir in glob('%s/*' % upload_dir):
             try:
                 if os.path.isdir(dir):
-                    dir_year = int(dir)
+                    dir_year = int(os.path.basename(dir))
                     returncode = upload(dir)
                     if returncode == 0 and dir_year < current_year:
-                        shutils.rmtree(dir)
+                        shutil.rmtree(dir)
             except:
-                print('Unable to process directory %s under %s' % (dir, upload_dir))
+                print("Unexpected error: %s " % traceback.format_exc())
+#                print('Unable to process directory %s under %s' % (dir, upload_dir))
         time.sleep(1)
 
     
